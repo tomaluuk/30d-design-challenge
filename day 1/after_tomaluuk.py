df95cfa250fe68b39764c38ecbf992d99b8eaf6e
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2023-07-13 16:14:13 +0000

    create first crude solution

--- day 1/after_tomaluuk.py
+++ day 1/after_tomaluuk.py
@@ -1,6 +1,18 @@
 def count_fruits(fruits: list[str]) -> dict[str, int]:
-    # your code goes here
-    pass
+    dict_counts = {}
+    
+    if len(fruits) == 0:
+        return {}
+
+    for fruit in fruits:
+        
+        #print(dict_counts.keys())
+        if not fruit in dict_counts.keys():
+            dict_counts[fruit] = 1
+        else:
+            dict_counts[fruit] += 1
+    #print(dict_counts)
+    return dict_counts
 
 
 def main() -> None:
