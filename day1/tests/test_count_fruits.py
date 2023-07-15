a86baae8cc2a4fda4f26f198008330cac5a36d60
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2023-07-15 10:32:01 +0000

    add test file

---
+++ day1/tests/test_count_fruits.py
@@ -0,0 +1,18 @@
+from day1.before import count_fruits 
+
+def test_list():
+    assert count_fruits(
+        [
+            "apple",
+            "banana",
+            "apple",
+            "cherry",
+            "banana",
+            "cherry",
+            "apple",
+            "apple",
+            "cherry",
+            "banana",
+            "cherry",
+        ]
+    ) == {"apple": 4, "banana": 3, "cherry": 4}
