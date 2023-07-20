6acc6bc5d52d76b7f6dc19c1be61e57526fca744
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2023-07-20 22:43:52 +0200

    Add tests

--- design_challenge/day1/tests/test_count_fruits.py
+++ design_challenge/day1/tests/test_count_fruits.py
@@ -1,6 +1,6 @@
 """Tests for count fruits function."""
 from design_challenge.day1.after_tomaluuk import count_fruits
-
+import pytest
 
 def test_list():
     assert count_fruits(
@@ -22,4 +22,17 @@ def test_list():
 
 def test_empty_list() -> None:
     """Test an empty input list."""
-    assert count_fruits(fruits=[]) == {}
+    assert count_fruits([]) == {}
+
+
+def test_invalid_input() -> None:
+    """Test an invalid input that is not a list"""
+    with pytest.raises(TypeError):
+        count_fruits(3)
+
+
+def test_omitted_input() -> None:
+    """Test an invalid input that is not a list"""
+    with pytest.raises(TypeError):
+        count_fruits()
+        
