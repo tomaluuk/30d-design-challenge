2339c8ff14fa8387844f075be09cae6409827c43
tomaluuk <30435626+tomaluuk@users.noreply.github.com>
2023-07-30 20:05:32 +0200

    Add quantity tests

--- design_challenge/day6/tests/shopping_cart/test_update_item.py
+++ design_challenge/day6/tests/shopping_cart/test_update_item.py
@@ -33,3 +33,24 @@ def test_invalid_price_update():
     cart = ShoppingCart(items)
     with pytest.raises(InvalidPriceException):
         cart.update_item("Book", price=Decimal(0.0))
+
+
+def test_quantity_update():
+    item_name = "Book"
+    items = [
+        Item(item_name, Decimal(14.5), 2),
+    ]
+    cart = ShoppingCart(items)
+
+    for _ in range(1, 1000):
+        cart.update_item(item_name, quantity=random.randint(1, 100))
+
+
+def test_invalid_quantity_update():
+    item_name = "Book"
+    items = [
+        Item(item_name, Decimal(14.5), 2),
+    ]
+    cart = ShoppingCart(items)
+    with pytest.raises(InvalidQuantityException):
+        cart.update_item("Book", quantity=-3)
